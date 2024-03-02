7f9c6b31277a405ef5a5d934e6177b658d644a6d
Akshay Waghmare <akshaywaghmare@Akshays-MacBook-Air.local>
2024-03-02 20:52:54 +0530

    avatar-webrtc-basic-model

---
+++ src/components/config.js
@@ -0,0 +1,12 @@
+
+export const avatarAppConfig = {
+    cogSvcRegion : "eastus",
+    cogSvcSubKey : "54a43ca6c8714a9aa22c9ffc10e54c04",
+    voiceName : "hi-IN-SwaraNeural",
+    avatarCharacter : "lisa",
+    avatarStyle : "casual-sitting",
+    avatarBackgroundColor : "#FFFFFFFF",
+    iceUrl : "stun:relay.communication.microsoft.com:3478",
+    iceUsername : "BQAANhqbDYAB2m4nhTmf9Xpc3OrbYS61nEz2tZ18cEEAAAAMARBZ51mjKP1J8bNClSDvDnz5kF4JJDlVnR5wzGdRd1wLmL2W67k=",
+    iceCredential : "PgVf3yQ4qlDwqeNd3kL9/YthSxU="
+  }
